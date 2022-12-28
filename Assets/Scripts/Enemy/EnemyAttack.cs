b3231600899a2a79e33347cbf094206ee1d69527
Jiren <gianghd@widogame.com>
2022-12-28 09:34:24 +0700

    version 1

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -6,7 +6,7 @@ public class EnemyAttack : MonoBehaviour
     public float timeBetweenAttacks = 0.5f;
     public int attackDamage = 10;
 
-
+ 
     Animator anim;
     GameObject player;
     PlayerHealth playerHealth;
